7c2207b09f9ae84476fb458bbd43931bbcb7529c
edinmukinovic <97086405+edinmukinovic@users.noreply.github.com>
2022-02-01 15:44:49 +0100

    Add files via upload

--- toast.js
+++ toast.js
@@ -1,54 +1,27 @@
-var i = 0;
-function move() {
-  if (i == 0) {
-    i = 1;
-    var elem = document.getElementById("myBar");
-    var width = 1;
-    var id = setInterval(frame, 50);
-    function frame() {
-      if (width >= 100) {
-        clearInterval(id);
-        i = 0;
-      } else {
-        width++;
-        elem.style.width = width + "%";
-      }
-    }
-  }
-}
-
-
-
-
 function edinToast(x) {
     var el = document.createElement("div");    
     var y = document.getElementById("toast-container");
     var c = document.createElement("closebtn");
-    var ProgressBar = document.getElementById("myBar");
+    var element = document.createElement('div');
 
     c.className="toast_close";
     el.className = "toast";
-    
- 
-    
+    element.className = "div"
+
     el.innerHTML = x;
     c.innerText = "X";
 
-
-    y.append(el, ProgressBar);
+    y.append(el, element);
     el.append(c);
-    
 
     el.className = "toast show";
-    setTimeout(function() {el.remove();}, 5000);
+
+    setTimeout(function() {el.remove(), element.remove();}, 5000);
 
     c.onclick = (event) => {
         el.remove();
-
+        element.remove();
         
-    };    
+    };   
+    
 }
-
-
-
-
